9b1fea7ee47f8a104d4b0d8a43d2f0e9ea6ae155
kosolap <kosolap.dmitriy@yandex.ru>
2016-09-01 17:08:08 +0300

    change 4-8 hw

--- js3/js/main.js
+++ js3/js/main.js
@@ -1,234 +1,241 @@
-var time= document.querySelector('.time');
-function timeFunc(){
-	var now = new Date();
-	time.innerHTML= now.getHours()+':'+ now.getMinutes();
-}
-timeFunc();
-setInterval(timeFunc, 60000);
+$(function(window, document) {
+    /*---------Вывод реального времени---------*/
+    var time = document.querySelector('.time');
 
-	var btn=document.querySelector('.btn-green');
-	btn.addEventListener('click', startFunc);
-	var lapBtn=document.querySelector('.btn-grey');
-	
-	
-
-	//JS Timer
-	var idTimer;
-	var timerEl =document.querySelector('.timer');
-	var timerMinutesEl=timerEl.querySelector('.minutes');
-	var timerSecondsEl=timerEl.querySelector('.seconds');
-	var timerMilSecondsEl=timerEl.querySelector('.milSeconds');
-	var milSecondTimer=1;
-	var SecondTimer=1;
-	var minutesTimer=1;
-
-	//JS Lap
-	var lapList=document.querySelector('.lap_list');
-
-	var idLap;
-	var lapEl =document.querySelector('.lap');
-	var lapMinutesEl=lapEl.querySelector('.minutes');
-	var lapSecondsEl=lapEl.querySelector('.seconds');
-	var lapMilSecondsEl=lapEl.querySelector('.milSeconds');
-	var milSecondlap=1;
-	var Secondlap=1;
-	var minuteslap=1;
-	var countLap=1;
+    function timeFunc() {
+        var now = new Date();
+        time.innerHTML = now.getHours() + ':' + now.getMinutes();
+    }
+    timeFunc();
+    setInterval(timeFunc, 60000);
+    /*----------------------------------------*/
 
 
 
-function startFunc() {
-	btn.className= 'btn btn-red';
-	btn.innerHTML = 'Стоп';
-	btn.removeEventListener('click', startFunc);
-	btn.addEventListener('click', pauseFunc);
+    /*------------Логика Секундомера----------*/
+    var btn = document.querySelector('.btn-green');
+    btn.addEventListener('click', startBtnFunc);
+    var lapBtn = document.querySelector('.btn-grey');
 
-	lapBtn.className= 'btn btn-black';
-	lapBtn.addEventListener('click', lapFunc);
+    var idIntervalSpotwatch;
+    var spotWatchBlock = document.querySelector('.timer');
+    var spotWatch = {
+        minSpan: spotWatchBlock.querySelector('.minutes'),
+        secSpan: spotWatchBlock.querySelector('.seconds'),
+        milSpan: spotWatchBlock.querySelector('.milSeconds'),
+        milSecond: 1,
+        second: 1,
+        minutes: 1,
+        reset: function() {
+            this.milSecond = this.second = this.minutes = 1;
+            this.minSpan.innerHTML = '00:';
+            this.secSpan.innerHTML = '00,';
+            this.milSpan.innerHTML = '00';
+        }
+    };
 
-	//start timer
-	idTimer=setInterval(timerStart, 10);
-	idLap=setInterval(lapStart, 10);
-}
+    var idIntervalLap;
+    var lapBlock = document.querySelector('.lap');
+    var lapSpotwatch = {
+        lapList: document.querySelector('.lap_list'),
+        minSpan: lapBlock.querySelector('.minutes'),
+        secSpan: lapBlock.querySelector('.seconds'),
+        milSpan: lapBlock.querySelector('.milSeconds'),
+        milSecond: 1,
+        second: 1,
+        minutes: 1,
+        countLap: 1,
+        reset: function() {
+            this.milSecond = this.second = this.minutes = 1;
+            this.minSpan.innerHTML = '00:';
+            this.secSpan.innerHTML = '00,';
+            this.milSpan.innerHTML = '00';
+        },
 
-function pauseFunc() {
-	btn.className= 'btn btn-green';
-	btn.innerHTML = 'Старт';
-	btn.removeEventListener('click', pauseFunc);
-	btn.addEventListener('click', continueFunc);
-
-	lapBtn.innerHTML ='Сброс';
-	lapBtn.removeEventListener('click', lapFunc);
-	lapBtn.addEventListener('click', resetFunc);
-
-	//clear timer
-	clearInterval(idTimer);
-	clearInterval(idLap);
-}
-
-function continueFunc() {
-	btn.className= 'btn btn-red';
-	btn.innerHTML='Стоп';
-	btn.removeEventListener('click', continueFunc);
-	btn.addEventListener('click', pauseFunc);
-
-	lapBtn.innerHTML ='Круг';
-	lapBtn.removeEventListener('click', resetFunc);
-	lapBtn.addEventListener('click', lapFunc);
-
-	//start timer
-	idTimer=setInterval(timerStart, 10);
-	idLap=setInterval(lapStart, 10);
-}
-function resetFunc() {
-	btn.removeEventListener('click', continueFunc);
-	btn.addEventListener('click', startFunc);
-
-	lapBtn.className ='btn btn-grey';
-	lapBtn.innerHTML = 'Круг';
-	lapBtn.removeEventListener('click', resetFunc);
-
-	//reset timer 
-	minutesTimer=SecondTimer=milSecondTimer=1;
-	timerMinutesEl.innerHTML='00:';
-	timerSecondsEl.innerHTML='00,';
-	timerMilSecondsEl.innerHTML='00';
-	// reset alp
-	lapList.innerHTML='';
-	minuteslap=Secondlap=milSecondlap=1;
-	lapMinutesEl.innerHTML='00:';
-	lapSecondsEl.innerHTML='00,';
-	lapMilSecondsEl.innerHTML='00';
-	countLap=1;
-
-}
+        resetAll: function() {
+        	this.reset();
+            this.countLap = 1;
+            this.lapList.innerHTML = '';
+        }
+    };
 
 
-function timerStart() {
-	if(milSecondTimer<10) timerMilSecondsEl.innerHTML='0'+milSecondTimer++;
-	else timerMilSecondsEl.innerHTML=milSecondTimer++;
-	if (milSecondTimer==100){
-		milSecondTimer=0;
-		if(SecondTimer<10) timerSecondsEl.innerHTML='0'+ SecondTimer++ + ',';
-		else if (SecondTimer<60){
-			timerSecondsEl.innerHTML=SecondTimer++ + ',';
-		}
-		else if (SecondTimer==60) {
-			SecondTimer=0;
-			timerSecondsEl.innerHTML='0'+ SecondTimer++ + ',';
-			if(minutesTimer<9) timerMinutesEl.innerHTML='0'+ ++minutesTimer + ':';
-			else timerMinutesEl.innerHTML= ++minutesTimer + ':';
-		}
-	}
-}
 
-function lapStart() {
-	if(milSecondlap<10) lapMilSecondsEl.innerHTML='0'+milSecondlap++;
-	else lapMilSecondsEl.innerHTML=milSecondlap++;
-	if (milSecondlap==100){
-		milSecondlap=0;
-		if(Secondlap<10) lapSecondsEl.innerHTML='0'+ Secondlap++ + ',';
-		else if (Secondlap<60){
-			lapSecondsEl.innerHTML=Secondlap++ + ',';
-		}
-		else if (Secondlap==60) {
-			Secondlap=0;
-			lapSecondsEl.innerHTML='0'+ Secondlap++ + ',';
-			if(minuteslap<9) lapMinutesEl.innerHTML='0'+ ++minuteslap + ':';
-			else lapMinutesEl.innerHTML= ++minuteslap + ':';
-		}
-	}
-}
+    function startBtnFunc() {
+        btn.className = 'btn btn-red';
+        btn.innerHTML = 'Стоп';
+        btn.removeEventListener('click', startBtnFunc);
+        btn.addEventListener('click', pauseBtnFunc);
 
-function lapFunc() {
-	var li=document.createElement('li');
+        lapBtn.className = 'btn btn-black';
+        lapBtn.addEventListener('click', addLap);
 
-	var lapNumber =document.createElement('span');
-	lapNumber.className='lap-count';
-	lapNumber.innerHTML='Круг '+countLap++;
-	var lapTime =document.createElement('span');
-	lapTime.className='lap-time';
-	lapTime.innerHTML= lapMinutesEl.innerHTML+lapSecondsEl.innerHTML+lapMilSecondsEl.innerHTML;
-	minuteslap=Secondlap=milSecondlap=1;
-	lapMinutesEl.innerHTML='00:';
-	lapSecondsEl.innerHTML='00,';
-	lapMilSecondsEl.innerHTML='00';
+        //start timer
+        idIntervalSpotwatch = setInterval(timerStart, 10);
+        idIntervalLap = setInterval(lapStart, 10);
+    }
 
-	li.appendChild(lapNumber);
-	li.appendChild(lapTime);
+    function pauseBtnFunc() {
+        btn.className = 'btn btn-green';
+        btn.innerHTML = 'Старт';
+        btn.removeEventListener('click', pauseBtnFunc);
+        btn.addEventListener('click', continueBtnFunc);
 
-	lapList.insertBefore(li, lapList.firstChild);
-}
+        lapBtn.innerHTML = 'Сброс';
+        lapBtn.removeEventListener('click', addLap);
+        lapBtn.addEventListener('click', resetBtnFunc);
+
+        //clear timer
+        clearInterval(idIntervalSpotwatch);
+        clearInterval(idIntervalLap);
+    }
+
+    function continueBtnFunc() {
+        btn.className = 'btn btn-red';
+        btn.innerHTML = 'Стоп';
+        btn.removeEventListener('click', continueBtnFunc);
+        btn.addEventListener('click', pauseBtnFunc);
+
+        lapBtn.innerHTML = 'Круг';
+        lapBtn.removeEventListener('click', resetBtnFunc);
+        lapBtn.addEventListener('click', addLap);
+
+        //start timer
+        idIntervalSpotwatch = setInterval(timerStart, 10);
+        idIntervalLap = setInterval(lapStart, 10);
+    }
+
+    function resetBtnFunc() {
+        btn.removeEventListener('click', continueBtnFunc);
+        btn.addEventListener('click', startBtnFunc);
+
+        lapBtn.className = 'btn btn-grey';
+        lapBtn.innerHTML = 'Круг';
+        lapBtn.removeEventListener('click', resetBtnFunc);
+
+        spotWatch.reset();
+        lapSpotwatch.resetAll();
+    }
+
+    function timerStart() {
+        if (spotWatch.milSecond < 10) spotWatch.milSpan.innerHTML = '0' + spotWatch.milSecond++;
+        else spotWatch.milSpan.innerHTML = spotWatch.milSecond++;
+        if (spotWatch.milSecond == 100) {
+            spotWatch.milSecond = 0;
+            if (spotWatch.second < 10) spotWatch.secSpan.innerHTML = '0' + spotWatch.second++ + ',';
+            else if (spotWatch.second < 60) {
+                spotWatch.secSpan.innerHTML = spotWatch.second++ + ',';
+            } else if (spotWatch.second == 60) {
+                spotWatch.second = 0;
+                spotWatch.secSpan.innerHTML = '0' + spotWatch.second++ + ',';
+                if (spotWatch.minutes < 9) spotWatch.minSpan.innerHTML = '0' + ++spotWatch.minutes + ':';
+                else spotWatch.minSpan.innerHTML = ++spotWatch.minutes + ':';
+            }
+        }
+    }
+
+    function lapStart() {
+        if (lapSpotwatch.milSecond < 10) lapSpotwatch.milSpan.innerHTML = '0' + lapSpotwatch.milSecond++;
+        else lapSpotwatch.milSpan.innerHTML = lapSpotwatch.milSecond++;
+        if (lapSpotwatch.milSecond == 100) {
+            lapSpotwatch.milSecond = 0;
+            if (lapSpotwatch.second < 10) lapSpotwatch.secSpan.innerHTML = '0' + lapSpotwatch.second++ + ',';
+            else if (lapSpotwatch.second < 60) {
+                lapSpotwatch.secSpan.innerHTML = lapSpotwatch.second++ + ',';
+            } else if (lapSpotwatch.second == 60) {
+                lapSpotwatch.second = 0;
+                lapSpotwatch.secSpan.innerHTML = '0' + lapSpotwatch.second++ + ',';
+                if (lapSpotwatch.minutes < 9) lapSpotwatch.minSpan.innerHTML = '0' + ++lapSpotwatch.minutes + ':';
+                else lapSpotwatch.minSpan.innerHTML = ++lapSpotwatch.minutes + ':';
+            }
+        }
+    }
+
+    function addLap() {
+        var li = document.createElement('li');
+
+        var lapNumberSpan = document.createElement('span');
+        lapNumberSpan.className = 'lap-count';
+        lapNumberSpan.innerHTML = 'Круг ' + lapSpotwatch.countLap++;
+        var lapTime = document.createElement('span');
+        lapTime.className = 'lap-time';
+        lapTime.innerHTML = lapSpotwatch.minSpan.innerHTML + lapSpotwatch.secSpan.innerHTML + lapSpotwatch.milSpan.innerHTML;
+        lapSpotwatch.reset();
+
+        li.appendChild(lapNumberSpan);
+        li.appendChild(lapTime);
+
+        lapSpotwatch.lapList.insertBefore(li, lapSpotwatch.lapList.firstChild);
+    }
+    /*----------------------------------------*/
+
+}(window, document));
 
 
-// jQuery 1
+/*----------Логика Табов------------------*/
 
 var $lastActiveNav = $('#stopwatch-nav');
-	$lastActiveNav.toggleClass(' nav--active');
-
+$lastActiveNav.toggleClass(' nav--active');
 var $lastDisplay = $("#displayStopwatch");
-
 var $titleName = $('.titleName')[0];
 
 function navClick(e) {
-		$lastActiveNav.toggleClass();
-		$lastActiveNav= $(e);
-		if($lastActiveNav[0].id=='alarm-nav')
-		{
-			$lastDisplay.css('display', 'none');
-			$lastDisplay=$('#displayAlarm');
-			$lastDisplay.css('display', 'block');
-			$titleName.innerHTML ='Будильник';
-		}
-		else if($lastActiveNav[0].id=='world-nav')
-		{
-			$lastDisplay.css('display', 'none');
-			$lastDisplay=$('#displayWorldTime');
-			$lastDisplay.css('display', 'block');
-			$titleName.innerHTML ='Мировые часы';
-		}
-		else if($lastActiveNav[0].id=='stopwatch-nav')
-		{
-			$lastDisplay.css('display', 'none');
-			$lastDisplay=$('#displayStopwatch');
-			$lastDisplay.css('display', 'block');
-			$titleName.innerHTML ='Секундомер';
-		}
-		else if($lastActiveNav[0].id=='timer-nav')
-		{
-			$lastDisplay.css('display', 'none');
-			$lastDisplay=$('#displayTimer');
-			$lastDisplay.css('display', 'block');
-			$titleName.innerHTML ='Таймер';
-		}
-		
-		$lastActiveNav.toggleClass(' nav--active');
-		
-		
-		// $lastActiveNav[0].className+=' nav--active';
-	}
+    $lastActiveNav.toggleClass();
+    $lastActiveNav = $(e);
+    if ($lastActiveNav[0].id == 'alarm-nav') {
+        $lastDisplay.css('display', 'none');
+        $lastDisplay = $('#displayAlarm');
+        $lastDisplay.css('display', 'block');
+        $titleName.innerHTML = 'Будильник';
+    } else if ($lastActiveNav[0].id == 'world-nav') {
+        $lastDisplay.css('display', 'none');
+        $lastDisplay = $('#displayWorldTime');
+        $lastDisplay.css('display', 'block');
+        $titleName.innerHTML = 'Мировые часы';
+    } else if ($lastActiveNav[0].id == 'stopwatch-nav') {
+        $lastDisplay.css('display', 'none');
+        $lastDisplay = $('#displayStopwatch');
+        $lastDisplay.css('display', 'block');
+        $titleName.innerHTML = 'Секундомер';
+    } else if ($lastActiveNav[0].id == 'timer-nav') {
+        $lastDisplay.css('display', 'none');
+        $lastDisplay = $('#displayTimer');
+        $lastDisplay.css('display', 'block');
+        $titleName.innerHTML = 'Таймер';
+    }
 
-// jQuery 2
+    $lastActiveNav.toggleClass(' nav--active');
+}
+/*----------------------------------------*/
 
+
+
+/*------Логика Выпадающих Подсказок-----*/
 var $labelFN = $('.firstname');
 $labelFN.hover(
-  function() {
-    $('.firstname-label').css('height', '14px');
-    // console.log($('.firstname-label'));
-  }, function() {
-    $('.firstname-label').css('height', '0');
-  }
-);
-var $labelLN = $('.lastname');
-$labelLN.hover(
-  function() {
-    $('.lastname-label').css('height', '14px');
-  }, function() {
-    $('.lastname-label').css('height', '0');
-  }
+    function() {
+        $('.firstname-label').css('height', '14px');
+    },
+    function() {
+        $('.firstname-label').css('height', '0');
+    }
 );
 
-function hiddenModal(){
-	var log= $('.login').css('display', 'none');
-	$('.iphone').css('display', 'block');
+var $labelLN = $('.lastname');
+$labelLN.hover(
+    function() {
+        $('.lastname-label').css('height', '14px');
+    },
+    function() {
+        $('.lastname-label').css('height', '0');
+    }
+);
+
+var $hidModClass = $('.hiddenModalClass');
+$hidModClass.click(hiddenModal);
+
+function hiddenModal() {
+    var log = $('.login').css('display', 'none');
+    $('.iphone').css('display', 'block');
 }
+/*----------------------------------------*/
