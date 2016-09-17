25760586502baa53b41b7f9d505a97e6208d9ca5
kosolap <kosolap.dmitriy@yandex.ru>
2016-09-17 11:06:10 +0300

    add home work js 15-16

--- js15_16/js/main.js
+++ js15_16/js/main.js
@@ -1,11 +1,8 @@
 $(function() {
-    var main = $('main');
-    var flickr_api = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
     var req = {
         tags: "",
         format: "json"
     }
-
     $('input[type="button"]').click(search);
     $('input[type="text"]').keyup(function(e) {
         if (e.keyCode == 13) {
@@ -13,13 +10,11 @@ $(function() {
         }
     });
     search();
-
     function search(e) {
         req.tags = $('input[type="text"]').val();
-        $.getJSON(flickr_api, req, function(data) {
+        $.getJSON('https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?', req, function(data) {
             console.log(data);
-
-            main.html(tmpl("item_tmpl", data));
+            $('main').html(tmpl("item_tmpl", data));
         });
     }
 });
