3973a37308faac8bc92aa1718bff01345dc047c6
Dmitry Kosolap <dkosolap@e3r6p9.unit.ua>
2017-09-01 17:37:10 +0300

    who enroy

--- unit.ua/js/main.js
+++ unit.ua/js/main.js
@@ -1,3 +1,10 @@
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').focus()
 })
+
+$('.modal').click(function(){
+  $('.stopVideo').each(function(){
+  	console.log("lalka");
+    this.contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*')
+  });
+});
