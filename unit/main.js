5efcc72968e40d895d703199d523ac5bb9d7316e
Dmitry Kosolap <dkosolap@e1r5p18.unit.ua>
2017-12-07 16:43:37 +0200

    lalka sasay

---
+++ unit/main.js
@@ -0,0 +1,87 @@
+var dynamicTabBar = window.dynamicTabBar = new mdc.tabs.MDCTabBar(document.querySelector('#dynamic-tab-bar'));
+var dots = document.querySelector('.dots');
+var panels = document.querySelector('.panels');
+
+dynamicTabBar.tabs.forEach(function(tab) {
+  tab.preventDefaultOnClick = true;
+});
+
+function updateDot(index) {
+  var activeDot = dots.querySelector('.dot.active');
+  if (activeDot) {
+    activeDot.classList.remove('active');
+  }
+  var newActiveDot = dots.querySelector('.dot:nth-child(' + (index + 1) + ')');
+  if (newActiveDot) {
+    newActiveDot.classList.add('active');
+  }
+}
+
+function updatePanel(index) {
+  var activePanel = panels.querySelector('.panel.active');
+  if (activePanel) {
+    activePanel.classList.remove('active');
+  }
+  var newActivePanel = panels.querySelector('.panel:nth-child(' + (index + 1) + ')');
+  if (newActivePanel) {
+    newActivePanel.classList.add('active');
+  }
+}
+
+dynamicTabBar.listen('MDCTabBar:change', function ({detail: tabs}) {
+  var nthChildIndex = tabs.activeTabIndex;
+
+  updatePanel(nthChildIndex);
+  updateDot(nthChildIndex);
+});
+
+function autoType(elementClass, typingSpeed){
+  var thhis = $(elementClass);
+  thhis.css({
+    "position": "relative",
+    "display": "inline-block"
+  });
+  thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
+  thhis = thhis.find(".text-js");
+  var text = thhis.text().trim().split('');
+  var amntOfChars = text.length;
+  var newString = "";
+  thhis.text("|");
+  setTimeout(function(){
+    thhis.css("opacity",1);
+    thhis.prev().removeAttr("style");
+    thhis.text("");
+    for(var i = 0; i < amntOfChars; i++){
+      (function(i,char){
+        setTimeout(function() {        
+          newString += char;
+          thhis.text(newString);
+        },i*typingSpeed);
+      })(i+1,text[i]);
+    }
+  },1500);
+}
+
+$(document).ready(function(){
+  // Now to start autoTyping just call the autoType function with the 
+  // class of outer div
+  // The second paramter is the speed between each letter is typed.   
+  autoType(".type-js",200);
+});
+
+// dots.addEventListener('click', function (evt) {
+//   if (!evt.target.classList.contains('dot')) {
+//     return;
+//   }
+
+//   evt.preventDefault();
+
+//   var dotIndex = [].slice.call(dots.querySelectorAll('.dot')).indexOf(evt.target);
+
+//   if (dotIndex >= 0) {
+//     dynamicTabBar.activeTabIndex = dotIndex;
+//   }
+
+//   updatePanel(dotIndex);
+//   updateDot(dotIndex);
+// })
