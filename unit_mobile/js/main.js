ad97fb4bef478e8243ee4146398ea0dac51d0a10
Denis Khlopov <dkhlopov@e1r4p16.unit.ua>
2018-01-24 19:22:38 +0200

    add some blocks

--- unit_mobile/js/main.js
+++ unit_mobile/js/main.js
@@ -20,21 +20,64 @@ $(window).on("scroll touchmove", function() {
 		if ($(document).scrollTop() < $(".main").position().top - 78) {
 			$('.logo-hidden').css("display", "none");
 			$('.nav-wrapper').removeClass('nav-bottom');
-			console.log(sroll - $(document).scrollTop());
+			// console.log(sroll - $(document).scrollTop());
 			$('.logo').css("width", sroll - $(document).scrollTop());
 			// $('.logo').css("width") = $('.logo').css("width") - ($(document).scrollTop() - $(".main").position().top);
 		}
 });
 
 
-			$(document).ready(function(){
-			  $(".owl-carousel").owlCarousel({
-				  items: 1,
-				  itemsDesktop : false,
-				itemsDesktopSmall : false,
-				itemsTablet: false,
-				itemsMobile : false,
-				animateOut: 'fadeOut',
-				  margin: 40,
-			  });
-			});
+$(document).ready(function(){
+	$(".owl-one").owlCarousel({
+		items: 1,
+		itemsDesktop : false,
+		itemsDesktopSmall : false,
+		itemsTablet: false,
+		itemsMobile : false,
+		nav: false,
+		animateOut: 'fadeOut',
+		margin: 20
+	});
+
+    $(".owl-two").owlCarousel({
+        items: 1,
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false,
+        animateOut: 'fadeOut',
+        margin: 40
+    });
+
+    $(".owl-three").owlCarousel({
+        items: 5,
+		loop: false,
+
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false,
+        animateOut: 'fadeOut',
+        margin: 5,
+		stagePadding: 50,
+        autoplay:false,
+        autoplayTimeout:5000,
+        responsive:{
+        	320:{
+        		items:1
+			},
+        	375:{
+        		items:2
+			},
+            600:{
+                items:5
+            }
+        }
+    });
+
+
+	$('.read-more-button').click(function(event) {
+        console.log($(this).parent().parent().find('.read-more-target').toggleClass('hide'));
+        console.log('click');
+    });
+});
